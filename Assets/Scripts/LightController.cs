b980c382881c0c3cfdebbbe8d5a00ecbccd74f4a
Tiago Martines <txm7803@rit.edu>
2015-09-17 14:56:34 -0400

    LightController and MovementBroadcast introduced.

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light2D))]
+public class LightController : MonoBehaviour {
+
+    Light2D light2d;
+
+	// Use this for initialization
+	void Awake () {
+        light2d = GetComponent<Light2D>();    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void UpdateLightFX()
+    {
+        light2d.UpdateLightFX();
+    }
+
+
+}
