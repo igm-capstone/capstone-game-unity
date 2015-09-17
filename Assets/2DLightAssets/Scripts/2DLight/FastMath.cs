fd99df61861201efeec0d5c8ef804a402deca570
Lucas Vasconcelos <garrafote@gmail.com>
2015-09-16 23:57:32 -0400

    Added Light 2D

---
+++ Assets/2DLightAssets/Scripts/2DLight/FastMath.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public static class FastMath
+{
+    private const int Bits = 7;
+
+    private const int Bits2 = Bits << 1;
+    private const int Mask = ~(-1 << Bits2);
+    private const int Count = Mask + 1;
+    private static readonly int Dim = (int)Mathf.Sqrt(Count);
+
+    private static readonly float InvDimMinus1 = 1.0f / (Dim - 1);
+
+    private const float Deg = 180.0f / Mathf.PI;
+
+    public  static float[] ATan2Array { get; private set; }
+
+    public static float[] SinArray { get; private set; }
+
+    public static float[] CosArray { get; private set; }
+
+    static FastMath()
+    {
+        SinArray = new float[360];
+        CosArray = new float[360];
+
+        for (int i = 0; i < 360; i++)
+        {
+            SinArray[i] = Mathf.Sin(i * Mathf.Deg2Rad);
+            CosArray[i] = Mathf.Cos(i * Mathf.Deg2Rad);
+        }
+
+        ATan2Array = new float[Count];
+        for (var i = 0; i < Dim; i++)
+        {
+            for (var j = 0; j < Dim; j++)
+            {
+                var x0 = (float)i / Dim;
+                var y0 = (float)j / Dim;
+
+                ATan2Array[j * Dim + i] = (float)Mathf.Atan2(y0, x0);
+            }
+        }
+    }
+
+    private const float PI = 3.14159265f;
+    private const float PIBy2 = 1.5707963f;
+    // |error| < 0.005
+    public static float Atan2(float y, float x)
+    {
+        if (x == 0.0f)
+        {
+            if (y > 0.0f) return PIBy2;
+            if (y == 0.0f) return 0.0f;
+            return -PIBy2;
+        }
+        float atan;
+        float z = y / x;
+        if (Mathf.Abs(z) < 1.0f)
+        {
+            atan = z / (1.0f + 0.28f * z * z);
+            if (x < 0.0f)
+            {
+                if (y < 0.0f) return atan - PI;
+                return atan + PI;
+            }
+        }
+        else
+        {
+            atan = PIBy2 - z / (z * z + 0.28f);
+            if (y < 0.0f) return atan - PI;
+        }
+        return atan;
+    }
+}
---
+++ Assets/2DLightAssets/Scripts/2DLight/Light2D.cs
@@ -0,0 +1,339 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class Light2D : MonoBehaviour {
+
+    enum VertexLocation
+    {
+        Middle = 0,
+        Left = -1, 
+        Right = 1,
+    }
+
+    struct Vertex
+    {
+        public float PseudoAngle;
+        public Vector3 Position;
+        public VertexLocation Location;
+        public bool IsEndpoint;
+    }
+    
+    public  int lightSegments = 8;
+    public  float lightRadius = 100f;
+    public Material lightMaterial;
+    public LayerMask shadowMask = Physics.DefaultRaycastLayers;
+
+    private PolygonCollider2D[] colliders;
+    private List<Vertex> vertices;
+
+    private Mesh lightMesh;
+
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+
+    void Awake()
+    {
+        lightMesh = new Mesh();
+        lightMesh.name = string.Format("Light Mesh ({0})", name);
+        lightMesh.MarkDynamic();
+
+        meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh = lightMesh;
+
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.sharedMaterial = lightMaterial;
+
+        vertices = new List<Vertex>();
+    }
+
+    void Update()
+    {
+        // TODO avoid getting all colliders every frame
+        FindLightColliders();
+        SetLight();
+        RenderLightMesh();
+        ResetBounds();
+    }
+
+    private void FindLightColliders()
+    {
+        // TODO filter colliders by LayerMask
+        colliders = FindObjectsOfType<PolygonCollider2D>();
+    }
+
+    private void SetLight()
+    {
+        var sortAngles = false;
+
+        // TODO consider using object pool
+        vertices.Clear();
+
+        // tolerance range for endpoint recognition
+        const float magRange = .15f;
+
+        var colliderVertices = new List<Vertex>();
+
+        // iterate meshes gathering vertices.
+        foreach (var collider in colliders)
+        {
+            colliderVertices.Clear();
+
+            var touchDown = false;
+            var touchUp = false;
+
+            // get polygon raycast vertices
+            foreach (var point in collider.points)
+            {
+                var vertex = new Vertex();
+
+                var worldPoint = collider.transform.TransformPoint(point);
+                var distance = (worldPoint - transform.position).magnitude;
+
+                var hit = Physics2D.Raycast(transform.position, worldPoint - transform.position, distance, shadowMask);
+                if (hit)
+                {
+                    vertex.Position = hit.point;
+
+                    vertex.IsEndpoint = Mathf.Abs(worldPoint.sqrMagnitude - hit.point.sqrMagnitude) <= magRange;
+                }
+                else
+                {
+                    vertex.Position = worldPoint;
+                    vertex.IsEndpoint = true;
+                }
+
+                Debug.DrawLine(transform.position, vertex.Position, vertex.IsEndpoint ? Color.red : Color.white);
+
+                // vertex position is saved in light local space.
+                vertex.Position = transform.InverseTransformPoint(vertex.Position);
+                vertex.PseudoAngle = PseudoAtan2(vertex.Position.y, vertex.Position.x);
+
+                if (vertex.PseudoAngle < 0f)
+                {
+                    touchDown = true;
+                }
+                else if (vertex.PseudoAngle > 2f)
+                {
+                    touchUp = true;
+                }
+
+
+                if (vertex.Position.sqrMagnitude <= lightRadius * lightRadius)
+                {
+                    colliderVertices.Add(vertex);
+                }
+            }
+
+            // Identify endpoints
+            if (colliderVertices.Count > 0)
+            {
+                // sort by angle in ASCENDING order
+                colliderVertices.Sort((v1, v2) => v1.PseudoAngle.CompareTo(v2.PseudoAngle));
+                
+                var hiloVertices = new Vertex[2];
+                var loIndex = 0;
+                var hiIndex = colliderVertices.Count - 1;
+
+                if (touchDown && touchUp)
+                {
+                    var lowest = float.MaxValue;
+                    var highest = float.MinValue;
+
+                    for (int i = 0; i < colliderVertices.Count; i++) 
+                    {
+                        var v = colliderVertices[i];
+
+                        // from all angles that range from -inf to 1
+                        // we need to find the highest angle (closest to 1)
+                        // and from all angles rannging from 1 to inf
+                        // we nned to find the lowest angle (closest to 1)
+                        if (v.PseudoAngle < 1f && v.PseudoAngle > highest)
+                        {
+                            highest = v.PseudoAngle;
+                            hiIndex = i;
+                        }
+                        else if (v.PseudoAngle > 1f && v.PseudoAngle < lowest)
+                        {
+                            lowest = v.PseudoAngle;
+                            loIndex = i;
+                        }
+                    }
+                }
+
+                Vertex vertex;
+                vertex = colliderVertices[loIndex];
+                vertex.Location = VertexLocation.Right;
+                hiloVertices[0] = vertex;
+                colliderVertices[loIndex] = vertex;
+
+                vertex = colliderVertices[hiIndex];
+                vertex.Location = VertexLocation.Left;
+                hiloVertices[1] = vertex;
+                colliderVertices[hiIndex] = vertex;
+
+                vertices.AddRange(colliderVertices);
+
+                foreach (var hiloVertex in hiloVertices)
+                {
+                    if (!hiloVertex.IsEndpoint)
+                    {
+                        continue;
+                    }
+
+                    var position = transform.TransformPoint(hiloVertex.Position);
+                    var distance = position - transform.position;
+                    var direction = distance.normalized;
+                    position += direction * .0001f;
+
+                    var hit = Physics2D.Raycast(position, direction, lightRadius, shadowMask);
+
+                    var newVertexPosition = hit ? (Vector3)hit.point
+                        : transform.TransformPoint(direction * lightRadius);
+
+                    Debug.DrawLine(position, newVertexPosition, Color.green);
+
+                    vertex = new Vertex();
+                    vertex.Position = transform.InverseTransformPoint(newVertexPosition);
+                    vertex.PseudoAngle = PseudoAtan2(vertex.Position.y, vertex.Position.x);
+
+                    vertices.Add(vertex);
+                }
+
+            }
+
+            
+
+        }
+
+        var theta = 0;
+        //float amount = (Mathf.PI * 2) / lightSegments;
+        var amount = 360 / lightSegments;
+
+        // Generate vectors for light cast
+        for (int i = 0; i < lightSegments; i++)
+        {
+            theta = (amount * i) % 360;
+
+            var vertex = new Vertex();
+            vertex.Position = new Vector3(FastMath.SinArray[theta], FastMath.CosArray[theta], 0);
+            vertex.PseudoAngle = PseudoAtan2(vertex.Position.y, vertex.Position.x);
+
+            vertex.Position *= lightRadius;
+            vertex.Position += transform.position;
+
+
+
+            var hit = Physics2D.Raycast(transform.position, vertex.Position - transform.position, lightRadius);
+            //Debug.DrawRay(transform.position, v.pos - transform.position, Color.white);
+
+            if (!hit)
+            {
+                //Debug.DrawLine(transform.position, v.pos, Color.white);
+                vertex.Position = transform.InverseTransformPoint(vertex.Position);
+                vertices.Add(vertex);
+            }
+        }
+
+        // sort all vertices by angle in a descending order
+        vertices.Sort((v1, v2) => v2.PseudoAngle.CompareTo(v1.PseudoAngle));
+
+
+        var epsilon = 0.00001f;
+        for (int i = 0; i < vertices.Count - 1; i++)
+        {
+
+            var fstVertex = vertices[i];
+            var sndVertex = vertices[i + 1];
+
+            if (Mathf.Abs(fstVertex.PseudoAngle - sndVertex.PseudoAngle) <= epsilon)
+            {
+
+                if (sndVertex.Location == VertexLocation.Right)
+                { // Right Ray
+
+                    if (fstVertex.Position.sqrMagnitude > sndVertex.Position.sqrMagnitude)
+                    {
+                        vertices[i] = sndVertex;
+                        vertices[i + 1] = fstVertex;
+                    }
+                }
+
+
+                // ALREADY DONE!!
+                if (fstVertex.Location == VertexLocation.Left)
+                { // Left Ray
+                    if (fstVertex.Position.sqrMagnitude < sndVertex.Position.sqrMagnitude)
+                    {
+
+                        vertices[i] = sndVertex;
+                        vertices[i + 1] = fstVertex;
+                    }
+                }
+            }
+        }
+    }
+
+    private void RenderLightMesh()
+    {
+        // fill the mesh with vertices
+        var meshVertices = new Vector3[vertices.Count + 1];
+        var meshUvs = new Vector2[meshVertices.Length];
+        var meshTriangles = new int[(vertices.Count * 3)];
+
+        meshVertices[0] = Vector3.zero;
+
+        // vertices
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            meshVertices[i + 1] = vertices[i].Position;
+        }
+
+        // uvs
+        for (int i = 0; i < meshVertices.Length; i++)
+        {
+            meshUvs[i] = meshVertices[i];
+        }
+
+        // triangles
+        for (int i = 0, j = 0, len = vertices.Count * 3; i < len; i += 3, j++)
+        {
+
+            meshTriangles[i] = 0;
+            meshTriangles[i + 1] = j + 1;
+            
+            // last index is 1 if is the last vertex (one loop)
+            meshTriangles[i + 2] = (i == len - 3) ? 1 : j + 2;
+        }
+
+        lightMesh.Clear();
+        lightMesh.vertices = meshVertices;
+        lightMesh.uv = meshUvs;
+        lightMesh.triangles = meshTriangles;
+
+        // update light material (in case it has changed)
+        meshRenderer.sharedMaterial = lightMaterial;
+    }
+
+    private void ResetBounds()
+    {
+        var bounds = lightMesh.bounds;
+        bounds.center = Vector3.zero;
+        lightMesh.bounds = bounds;
+    }
+
+    // This method emulates Atan2. 
+    // Although the output angle is highly innacurate this
+    // can be used as a pseudo angle comparator.
+    static float PseudoAtan2(float dy, float dx)
+    {
+        float ax = Mathf.Abs(dx);
+        float ay = Mathf.Abs(dy);
+        float p = dy / (ax + ay);
+        return dx < 0 ? 2 - p : p;
+    }
+}
