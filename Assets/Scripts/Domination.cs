6703f7db0e1887399a4b589bbc1e6bf9d0f2f31f
Karan Sahu <karansahu1991@gmail.com>
2015-11-24 19:15:38 -0500

    Domination Point prefab done. Avatar walks in and stays in the box for a specified time and captures it. There's a slider to show progress. If he walks out before being captured, the slider drops down.

---
+++ Assets/Scripts/Domination.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Domination : MonoBehaviour
+{
+    [System.NonSerialized]
+    public bool captured = false;
+    private bool outsideDominationArea = true;
+    public float elapsedTime = 0.0f;
+    public float timeToCapture = 20.0f;
+    private GameObject dominationSlider;
+
+    public void Awake()
+    {
+        dominationSlider = transform.FindChild("Canvas").transform.FindChild("DominationSlider").gameObject;
+    }
+
+    public void Update()
+    {
+        if (outsideDominationArea == true && elapsedTime >= 0.0f)
+        {
+            elapsedTime -= Time.deltaTime;
+            dominationSlider.GetComponent<Slider>().value = elapsedTime / timeToCapture;
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            outsideDominationArea = false;
+            if (elapsedTime < timeToCapture)
+            {
+                elapsedTime += Time.deltaTime;
+                dominationSlider.GetComponent<Slider>().value = elapsedTime / timeToCapture;
+            }
+            else
+            {
+                captured = true;
+            }
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player" && captured == false)
+        {
+            outsideDominationArea = true;
+        }
+    }
+}
