7a13620be5dc196722782ed6420667a9a387db20
Henrique Chaltein <hxs1151@rit.edu>
2015-11-24 20:01:41 -0500

    Updated Domination Point PreFab

--- Assets/Scripts/Domination.cs
+++ Assets/Scripts/Domination.cs
@@ -9,19 +9,14 @@ public class Domination : MonoBehaviour
     private bool outsideDominationArea = true;
     public float elapsedTime = 0.0f;
     public float timeToCapture = 20.0f;
-    private GameObject dominationSlider;
-
-    public void Awake()
-    {
-        dominationSlider = transform.FindChild("Canvas").transform.FindChild("DominationSlider").gameObject;
-    }
+    public Slider dominationSlider;
 
     public void Update()
     {
         if (outsideDominationArea == true && elapsedTime >= 0.0f)
         {
             elapsedTime -= Time.deltaTime;
-            dominationSlider.GetComponent<Slider>().value = elapsedTime / timeToCapture;
+            dominationSlider.value = elapsedTime / timeToCapture;
         }
     }
     void OnTriggerStay2D(Collider2D other)
@@ -32,7 +27,7 @@ public class Domination : MonoBehaviour
             if (elapsedTime < timeToCapture)
             {
                 elapsedTime += Time.deltaTime;
-                dominationSlider.GetComponent<Slider>().value = elapsedTime / timeToCapture;
+                dominationSlider.value = elapsedTime / timeToCapture;
             }
             else
             {
