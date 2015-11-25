87a5ed4106e9f6a3ae53a5021f3d9a337d6cf992
Gabriel Ortega <gabriel.ortega56@gmail.com>
2015-11-24 19:31:35 -0500

    Melee Attack.

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -13,8 +13,11 @@ public class AvatarController : NetworkBehaviour
     private float collisionRadius;
     private Collider2D[] moveableObstacle = new Collider2D[1];
 
+    MeleeWeaponBehavior weaponBehavior;
+
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        weaponBehavior = GetComponentInChildren<MeleeWeaponBehavior>();
 	}
 	
 	void FixedUpdate () 
@@ -35,6 +38,11 @@ public class AvatarController : NetworkBehaviour
         {
             Application.Quit();
         }
+
+        if (Input.GetKey(KeyCode.M))
+        {
+            weaponBehavior.StartCoroutine(weaponBehavior.Slash());
+        }
     }
 
     private void move()
@@ -52,6 +60,11 @@ public class AvatarController : NetworkBehaviour
             moveableObstacle[0].transform.parent = gameObject.transform;
         else
             moveableObstacle[0].transform.parent = null;
+    }
+
+    private void MeleeAttack()
+    {
+
     }
 
     #if UNITY_EDITOR
