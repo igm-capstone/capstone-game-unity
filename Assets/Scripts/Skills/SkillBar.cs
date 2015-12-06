4c8d455df0d8311d7c0a951797a8fb2f9280ea7f
Gabriel Ortega <gabriel.ortega56@gmail.com>
2015-12-05 19:34:53 -0500

    Started work on Pickups.

--- Assets/Scripts/Skills/SkillBar.cs
+++ Assets/Scripts/Skills/SkillBar.cs
@@ -12,15 +12,18 @@ public class SkillBar : MonoBehaviour
         UseSkill
     }
 
+    public int MaxSkills;
     public bool HideInactiveSkills = false;
     public OnClickBehaviorType OnClickBehavior;
 
-    private ISkill[] _skillList;
+    private List<ISkill> _skillList;
     private ISkill _activeSkill = null;
+    private int _skillCapacity;
 
     void Awake()
     {
-        _skillList = transform.parent.GetComponentsInChildren<ISkill>();
+        SetSkillCapacity(MaxSkills);
+        SetSkillList(transform.parent.GetComponentsInChildren<ISkill>().ToList<ISkill>());
     }
 
     public ISkill GetActiveSkill()
@@ -38,5 +41,56 @@ public class SkillBar : MonoBehaviour
                 s.SetHighlight(s == skill);
         }
         _activeSkill = skill;
+    }
+
+    public void SetSkillList(List<ISkill> skillList)
+    {
+        if (skillList.Count() < _skillCapacity)
+        {
+            _skillList = skillList;
+        }
+    }
+
+    public void ClearSkillList()
+    {
+        _skillList.Clear();
+    }
+
+    public void AddSkill(ISkill skill)
+    {
+        if (_skillList.Count < _skillCapacity)
+        {
+            _skillList.Add(skill);
+        }
+    }
+
+    public ISkill GetSkill(int index)
+    {
+        return _skillList[index];
+    }
+
+    public void RemoveSkill(ISkill skill)
+    {
+        _skillList.Remove(skill);
+    }
+
+    public int GetSkillCount()
+    {
+        return _skillList.Count();
+    }
+
+    public int GetSkillCapacity()
+    {
+        return _skillCapacity;
+    }
+
+    public void SetSkillCapacity(int capacity)
+    {
+        _skillCapacity = capacity;
+    }
+
+    public bool IsFull()
+    {
+        return _skillList.Count() == _skillCapacity;
     }
 }
