23d26431a1a57b5088f77fa3f472f340cb39d537
Henrique Chaltein <hxs1151@rit.edu>
2016-02-02 12:10:19 -0500

    Limited Window movement based on the level size.

--- Assets/Scripts/GhostCamController.cs
+++ Assets/Scripts/GhostCamController.cs
@@ -1,70 +1,107 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GhostCamController : MonoBehaviour {
-
-
-    public bool isMouseOnScreen;
+// Script that contols the Ghost Sliding windonw. The move speed depends on the FrameRate.
+public class GhostCamController : MonoBehaviour
+{
+    public GameObject OuterWallsArray;
     float SpdXAxis, SpdYAxis;
+    float HorBrdr, VerBrdr;
     float ScrRightBorder, ScrLeftBorder, ScrUpBorder, ScrDownBorder;
 
     Transform GhostTransform;
+
+    Camera GhostCam;
+
+    Bounds LvlBounds;
+
+    Vector3 LeftStep;
+    Vector3 RightStep;
+    Vector3 DownStep;
+    Vector3 UpStep;
 
     // Use this for initialization
     void Start ()
     {
         // Object Getters
         GhostTransform = transform.parent.transform;
+        GhostCam = GetComponent<Camera>();
+        GhostCam.orthographicSize = 20.0f;
+
+        // Get Lvl bounds
+        OuterWallsArray = GameObject.Find("OuterWalls");
+        LvlBounds = CreateLevelBounds(OuterWallsArray);
 
         // Starting Values
+        // Speed
         SpdXAxis = 1.2f;
         SpdYAxis = 1.2f;
+
+        // Position Steps
+        LeftStep = new Vector3(-SpdXAxis, 0);
+        RightStep = new Vector3(SpdXAxis, 0);
+        DownStep = new Vector3(0, -SpdYAxis);
+        UpStep = new Vector3(0, SpdYAxis);
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
         // This calculation is done here to account for the window changing sizes
+        // These numbers are in pixels (screen space)
         ScrRightBorder = Screen.width - 10.0f;
         ScrLeftBorder = 10.0f;
         ScrUpBorder = Screen.height - 10.0f;
         ScrDownBorder = 10.0f;
 
-
-        Debug.Log(Input.mousePosition);
         // Mouse movement on X-axis of the screen
         // Left
         if (Input.mousePosition.x < ScrLeftBorder)
-            GhostTransform.position += new Vector3(-SpdXAxis, 0);
+        {
+            // If Screen can move, move!
+            if (LvlBounds.Contains(GhostTransform.position + 10*LeftStep))
+                GhostTransform.position += LeftStep;
+        }
         // Right
-        else if(Input.mousePosition.x > ScrRightBorder)
-            GhostTransform.position += new Vector3(SpdXAxis, 0);
-        
+        else if (Input.mousePosition.x > ScrRightBorder)
+        {
+            if (LvlBounds.Contains(GhostTransform.position + 10*RightStep))
+                GhostTransform.position += RightStep;
+        }
+
         // Mouse movement on Y-axis of the screen
+
         // Down
         if (Input.mousePosition.y < ScrDownBorder)
-            GhostTransform.position += new Vector3(0, -SpdXAxis);
+        {
+            if (LvlBounds.Contains(GhostTransform.position + 10*DownStep))
+                GhostTransform.position += DownStep;
+        }
         // Up
         else if (Input.mousePosition.y > ScrUpBorder)
-            GhostTransform.position += new Vector3(0, SpdXAxis);
+        {
+            if (LvlBounds.Contains(GhostTransform.position + 10*UpStep))
+                GhostTransform.position += UpStep;
+        }
+    }
 
-        /*
-                if (GhostTransform.position.x > RightLimit)
-                {
-                    GhostTransform.position = new Vector3(RightLimit, GhostTransform.position.y);
-                }
-                if (GhostTransform.position.x < LeftLimit)
-                {
-                    GhostTransform.position = new Vector3(LeftLimit, GhostTransform.position.y);
-                }
-                if (GhostTransform.position.x > UpLimit)
-                {
-                    GhostTransform.position = new Vector3(GhostTransform.position.x, UpLimit);
-                }
-                if (GhostTransform.position.x < DownLimit)
-                {
-                    GhostTransform.position = new Vector3(GhostTransform.position.x, DownLimit);
-                }
-                */
+    // Searches for the outer Walls
+    Bounds CreateLevelBounds (GameObject OuterWalls)
+    {
+        Bounds WallBounds;
+        Renderer StartRend;
+        // Gets all renderer bounds from the outer walls
+        var WallRends = OuterWalls.GetComponentsInChildren<Renderer>();
+        WallBounds = WallRends[0].bounds;
+        
+        // Starting Bounds to encapsulate others.
+        StartRend = WallRends[0];
+        foreach (Renderer curRend in WallRends)
+        {
+            if (curRend.bounds != StartRend.bounds)
+                WallBounds.Encapsulate(curRend.bounds);
+        }
+
+        return WallBounds;
     }
 }
