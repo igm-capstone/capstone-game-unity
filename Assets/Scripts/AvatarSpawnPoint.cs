252f0537b38d4e4f925d9b620f9a1a1dafd53d39
Lucas Vasconcelos <garrafote@gmail.com>
2016-02-03 19:02:43 -0500

    Added rig3d collection component to moveable blocks

--- Assets/Scripts/AvatarSpawnPoint.cs
+++ Assets/Scripts/AvatarSpawnPoint.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[Rig3DAsset("spawnPoint", Rig3DExports.Position)]
 public class AvatarSpawnPoint : MonoBehaviour {
     [Range(1,4)]
     public int PlayerID = 1;
