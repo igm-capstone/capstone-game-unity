4293e6c043bf5e069738677e682cf7762f1e58c0
Tiago Martines <txm7803@rit.edu>
2016-03-03 11:22:49 -0500

    Custom, SyncTransform / SyncRotationChild

---
+++ Assets/Scripts/Network/SyncPosition.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+[NetworkSettings(channel = 0, sendInterval = 0.1f)]
+public class SyncPosition : NetworkBehaviour
+{
+
+    private Vector3 syncPos;
+
+    [SerializeField]
+    Transform myTransform;
+    private float lerpRate;
+    private float normalLerpRate = 24;
+    private float fasterLerpRate = 40;
+
+    private Vector3 lastPos;
+    private float threshold = 0.5f;
+
+    private List<Vector3> syncPosList = new List<Vector3>();
+    [SerializeField]
+    private bool useHistoricalLerping = false;
+    private float closeEnough = 0.20f;
+
+    void Start()
+    {
+        lerpRate = normalLerpRate;
+    }
+
+    void Update()
+    {
+        LerpPosition();
+    }
+
+    void FixedUpdate()
+    {
+        TransmitPosition();
+    }
+
+    void LerpPosition()
+    {
+        if (hasAuthority) return;
+
+        if (useHistoricalLerping)
+        {
+            HistoricalLerping();
+        }
+        else
+        {
+            OrdinaryLerping();
+        }
+    }
+
+
+    void TransmitPosition()
+    {
+        if (!hasAuthority) return;
+        if (Vector3.Distance(myTransform.position, lastPos) > threshold)
+        {
+            CmdProvidePositionToServer(myTransform.position);
+            lastPos = myTransform.position;
+            Debug.Log("Command sent");
+        }
+    }
+
+    [Command]
+    void CmdProvidePositionToServer(Vector3 pos)
+    {
+        RpcSyncTransform(pos);
+        Debug.Log("Command rcv");
+    }
+
+
+    [ClientRpc]
+    void RpcSyncTransform(Vector3 latestPos)
+    {
+        syncPos = latestPos;
+        syncPosList.Add(syncPos);
+        Debug.Log("Rpc rcv");
+    }
+
+    void OrdinaryLerping()
+    {
+        myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+    }
+
+    void HistoricalLerping()
+    {
+        if (syncPosList.Count > 0)
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPosList[0], Time.deltaTime * lerpRate);
+
+            if (Vector3.Distance(myTransform.position, syncPosList[0]) < closeEnough)
+            {
+                syncPosList.RemoveAt(0);
+            }
+
+            if (syncPosList.Count > 10)
+            {
+                lerpRate = fasterLerpRate;
+            }
+            else
+            {
+                lerpRate = normalLerpRate;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Network/SyncRotationChild.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+[NetworkSettings(channel = 0, sendInterval = 0.1f)]
+public class SyncRotationChild : NetworkBehaviour
+{
+
+    private float syncPlayerRotation;
+
+    [SerializeField]
+    private Transform playerTransform;
+    private float lerpRate = 20;
+
+    private float lastPlayerRot;
+    private float threshold = 5.0f;
+
+    private List<float> syncPlayerRotList = new List<float>();
+    private float closeEnough = 10.0f;
+    [SerializeField]
+    private bool useHistoricalInterpolation;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        LerpRotations();
+    }
+
+    void FixedUpdate()
+    {
+        TransmitRotations();
+    }
+
+    void LerpRotations()
+    {
+        if (hasAuthority) return;
+        if (useHistoricalInterpolation)
+        {
+            HistoricalInterpolation();
+        }
+        else
+        {
+            OrdinaryLerping();
+        }
+    }
+
+    void TransmitRotations()
+    {
+        if (!hasAuthority) return;
+        if ((Mathf.Abs(playerTransform.localEulerAngles.z - lastPlayerRot) > threshold))
+        {
+            CmdProvidePositionToServer(playerTransform.localEulerAngles.z);
+            lastPlayerRot = playerTransform.localEulerAngles.z;
+            Debug.Log("Command tcp");
+        }
+    }
+
+    [Command]
+    void CmdProvidePositionToServer(float angle)
+    {
+        RpcSyncRotation(angle);
+        Debug.Log("Command rcv");
+    }
+
+
+    [ClientRpc]
+
+    void RpcSyncRotation(float latestPlayerRot)
+    {
+        syncPlayerRotation = latestPlayerRot;
+        syncPlayerRotList.Add(syncPlayerRotation);
+        Debug.Log("Rpc rcv");
+    }
+
+    void HistoricalInterpolation()
+    {
+        if (syncPlayerRotList.Count > 0)
+        {
+            Vector3 playerNewRot = new Vector3(0, 0, syncPlayerRotList[0]);
+            playerTransform.rotation = Quaternion.Lerp(playerTransform.rotation, Quaternion.Euler(playerNewRot), lerpRate * Time.deltaTime);
+
+            if (Mathf.Abs(playerTransform.localEulerAngles.z - syncPlayerRotList[0]) < closeEnough)
+            {
+                syncPlayerRotList.RemoveAt(0);
+            }
+        }
+    }
+
+    void OrdinaryLerping()
+    {
+        Vector3 playerNewRot = new Vector3(0, 0, syncPlayerRotation);
+        playerTransform.rotation = Quaternion.Lerp(playerTransform.rotation, Quaternion.Euler(playerNewRot), lerpRate * Time.deltaTime);
+
+    }
+}
