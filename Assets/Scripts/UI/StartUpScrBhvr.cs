75fd1243d4a336967e629be6781175113bcfa41f
Henrique Chaltein <hxs1151@rit.edu>
2016-04-09 12:44:04 -0400

    Started the Window. made scripts and planned them out.

---
+++ Assets/Scripts/UI/StartUpScrBhvr.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartUpScrBhvr : MonoBehaviour {
+
+    public Button RdyBtn;
+    public Text ClassName;
+    
+    // Use this for initialization
+	void Start()
+    {
+        RdyBtn = transform.Find("ReadyButton").GetComponent<Button>();
+        ClassName = transform.Find("ClassName").GetComponent<Text>();
+	}
+}
---
+++ Assets/Scripts/UI/StartUpScreenMngr.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum PlyrNum
+{
+    Ghost,
+    Sprinter,
+    TrapMaster,
+    Support
+}
+
+public class StartUpScreenMngr : MonoBehaviour
+{
+    int MyPlayerInt;
+
+    GameObject MainCanvasRef;
+    GameObject PlayerObjRef;
+
+    [SerializeField]
+    GameObject StrtWndwPreFab;
+
+    PlyrNum MyPlyrId;
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayerObjRef = this.gameObject;
+
+        // Disable this script for all player classes that are not "Me" and exits.
+        if (PlayerObjRef.name != "Me")
+        {
+            this.enabled = false;
+            return;
+        }
+
+        // Gets a reference for the main canvas
+        MainCanvasRef = GameObject.Find("MainCanvas");
+
+        // Find out what class the player is playing.
+        if (PlayerObjRef.GetComponent<GhostController>())
+        {
+            // Disable movement
+            PlayerObjRef.GetComponent<GhostController>().enabled = false;
+            MyPlyrId = PlyrNum.Ghost;
+        }
+        else
+        {
+            // Disable movement
+            PlayerObjRef.GetComponent<AvatarController>().enabled = false;
+            if (PlayerObjRef.GetComponent<Sprint>())
+            {
+                MyPlyrId = PlyrNum.Sprinter;
+            }
+            else if (PlayerObjRef.GetComponent<Heal>())
+            {
+                MyPlyrId = PlyrNum.Support;
+            }
+            else if (PlayerObjRef.GetComponent<SetTrapPoison>())
+            {
+                MyPlyrId = PlyrNum.TrapMaster;
+            }
+        }
+
+        MyPlayerInt = (int)MyPlyrId;
+
+        // Instance the Start Up window.
+
+        GameObject window = (GameObject)Instantiate(StrtWndwPreFab,Vector3.zero,Quaternion.identity);
+
+        // Set window parent and center it.
+        window.transform.SetParent(MainCanvasRef.transform);
+        window.transform.localPosition = Vector3.zero;
+        window.transform.localScale= Vector3.one;
+
+        switch (MyPlyrId)
+        {
+            case PlyrNum.Ghost:
+                window.GetComponent<StartUpScrBhvr>().ClassName.text = "Ghost";
+                break;
+
+            case PlyrNum.Sprinter:
+                window.GetComponent<StartUpScrBhvr>().ClassName.text = "Sprinter";
+                break;
+
+            case PlyrNum.TrapMaster:
+                window.GetComponent<StartUpScrBhvr>().ClassName.text = "TrapMaster";
+                break;
+
+            case PlyrNum.Support:
+                window.GetComponent<StartUpScrBhvr>().ClassName.text = "Support";
+                break;
+
+            default:
+                break;
+        }
+        
+
+
+    }
+
+    // TEST CODE.
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameStart();
+        }
+    }
+    // END OF TEST CODE
+
+    // ReadyBtnfunction
+
+    // StartTheGame function
+    public void GameStart()
+    {
+        // DeletesStart up window instance
+
+        // Enable movement
+        switch (MyPlyrId)
+        {
+            case PlyrNum.Ghost:
+                PlayerObjRef.GetComponent<GhostController>().enabled = true;
+                break;
+
+            default:
+                PlayerObjRef.GetComponent<AvatarController>().enabled = true;
+                break;
+        }
+    }
+}
