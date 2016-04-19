a77351b5f040469a61fe4186e21501d34e8e6cef
Karan Sahu <karansahu1991@gmail.com>
2016-04-19 18:53:16 -0400

    New models. Updated old ones with new UVs and textures. Added new models to scene

---
+++ Assets/Editor/ChangeFirstChildMaterial.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ChangeFirstChildMaterial : MonoBehaviour {
+
+	[MenuItem("CONTEXT/Transform/Change to Bathroom Floor")]
+    private static void BathFloor(MenuCommand mCommand)
+    {
+        var goTransform = mCommand.context as Transform;
+        goTransform.GetChild(0).GetComponent<MeshRenderer>().material = Resources.Load("BathroomFloor") as Material;
+    }
+
+    [MenuItem("CONTEXT/Transform/Change to Basement Floor")]
+    private static void BasementFloor(MenuCommand mCommand)
+    {
+        var goTransform = mCommand.context as Transform;
+        goTransform.GetChild(0).GetComponent<MeshRenderer>().material = Resources.Load("BasementFloor") as Material;
+    }
+
+    [MenuItem("CONTEXT/Transform/Change to Wood Floor")]
+    private static void WoodFloor(MenuCommand mCommand)
+    {
+        var goTransform = mCommand.context as Transform;
+        goTransform.GetChild(0).GetComponent<MeshRenderer>().material = Resources.Load("WoodFloor") as Material;
+    }
+}
