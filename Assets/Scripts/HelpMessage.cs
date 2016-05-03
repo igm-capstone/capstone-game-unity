45e30e73c622ba1f32feed6487e69fe7fcda213e
Karan Sahu <karansahu1991@gmail.com>
2016-05-03 16:11:27 -0400

    Main Menu revamp

--- Assets/Scripts/HelpMessage.cs
+++ Assets/Scripts/HelpMessage.cs
@@ -9,28 +9,45 @@ public class HelpMessage : MonoBehaviour
     private Text _text;
     private Image _panel;
     private float _lastSet;
+    private bool setMsg = false;
 
     public static HelpMessage Instance { get; private set; }
+
+    Color textColor;
+    Color panelColor;
 
     // Use this for initialization
     void Awake ()
     {
         Instance = this;
         _text = GetComponentInChildren<Text>();
-        _panel = GetComponentInChildren<Image>();
+        _panel = GetComponentInChildren<Image>();       
+
+        textColor = _text.color;
+        panelColor = _panel.color;
     }
 
     void Update()
     {
+        if(setMsg)
+        {
+            //Debug.Log(textColor.a);
+            textColor.a += 0.1f;
+            _text.color = textColor;
+
+            panelColor.a += 0.1f;
+            _panel.color = panelColor;
+        }
+
         if (_lastSet + TimeOut < Time.time)
         {
-            Color color = _text.color;
-            color.a -= .1f;
-            _text.color = color;
+            textColor.a -= 0.06f;
+            _text.color = textColor;
 
-            color = _panel.color;
-            color.a -= 0.06f;
-            _panel.color = color;
+            panelColor.a -= 0.06f;
+            _panel.color = panelColor;
+
+            setMsg = false; 
         }
     }
 
@@ -43,15 +60,12 @@ public class HelpMessage : MonoBehaviour
         } 
     
         _text.text = msg;
-
-        Color color = _text.color;
-        color.a = 1.0f;
-        _text.color = color;
-
-        color = _panel.color;
-        color.a = 0.6f;
-        _panel.color = color;
-
+        if(!setMsg)
+        {
+            textColor.a = 0;
+            panelColor.a = 0;
+        }
+        setMsg = true;
 	    _lastSet = Time.time;
 	}
 }
